75debdf95b3b045db746a8f9002843e18c7a94d4
mikivan1980 <mikivan1980@mail.ru>
2017-08-21 23:13:21 +0300

    модуль в разработке

--- newData.js
+++ newData.js
@@ -1,5 +1,11 @@
 
-const C = require('./constants');
+const C    = require('./constants');
+const Dict = require('./dictionary');
+
+
+function paddingLeft(paddingValue, string) {
+    return String(paddingValue + string).slice(-paddingValue.length);
+};
 
 
 
@@ -7,61 +13,93 @@ class DataElement {
     constructor( metod, a, b, c, d, e, f){
 
         this.isDataElement = false;
+        this.discription   = null;
 
-        this.group = null;
-        this.element = null;
-        this.vr = null;
-        this.vm = null;
-        this.length = null;
+        this.group    = null;
+        this.element  = null;
+        this.vr       = null;
+        this.vm       = null;
+        this.length   = null;
 
-        this.syntax = null;
-        this.endian = null;
+        this.syntax   = null;
+        this.endian   = null;
         this.implicit = null;
 
-        this.field = null;
+        this.field    = null;
 
-        if( metod === 'Read' ) this.DataElementCreateByRead(a, b);
+        if( metod === 'Read' ) this.DataElementCreateByRead(a,b);
         if( metod === 'Hand' ) this.DataElementcreateByHand(a,b,c,d,e,f);
         else this.exit();
 
     }
 
-    DataElementCreateByRead( a, b ){ // strean, syntax
+    DataElementCreateByRead( stream, syntax ){
 
-        this.syntax = b;
-        this.implicit = (this.syntax === C.IMPLICIT_LITTLE_ENDIAN) ? true : false;
-        this.endian = (this.syntax === C.IMPLICIT_LITTLE_ENDIAN || this.syntax === C.EXPLICIT_LITTLE_ENDIAN) ? C.LITTLE_ENDIAN : C.BIG_ENDIAN;
+        this.isDataElement = false;
 
-        this.group = a.read(C.TYPE_UINT16);
-        this.element = a.read(C.TYPE_UINT16);
+        //если syntax не задан, по умолчанию - DICOM Implicit VR Little Endian Transfer Syntax!!!
+          /*                                   */    this.syntax = C.IMPLICIT_LITTLE_ENDIAN;  this.implicit = true;  this.endian = C.LITTLE_ENDIAN;
+        if( syntax === C.EXPLICIT_LITTLE_ENDIAN )  { this.syntax = C.EXPLICIT_LITTLE_ENDIAN;  this.implicit = false; this.endian = C.LITTLE_ENDIAN; }
+        if( syntax === C.EXPLICIT_BIG_ENDIAN    )  { this.syntax = C.EXPLICIT_BIG_ENDIAN;     this.implicit = false; this.endian = C.BIG_ENDIAN; }
+
+
+        let oldEndian = stream.endian;
+        stream.setEndian(this.endian);
+
+        this.group   =  paddingLeft("0000", stream.read(C.TYPE_UINT16).toString(16));
+        this.element =  paddingLeft("0000", stream.read(C.TYPE_UINT16).toString(16));
+
+        if(this.implicit){
+            let dictEntry = Dict.dicomNDict['group']['element'];
+            this.vr = dictEntry.vr;
+            this.vm = dictEntry.vm;
+            this.discription = dictEntry.keyword;
+            this.length = stream.read(C.TYPE_UINT32);
+
+            this.field = new Buffer();
+
+        }
+        else{
+
+
+        }
 
 
 
-        console.log('read' + a + b );
+
+        stream.setEndian(oldEndian);
+
+       // console.log('read' + a + b );
 
     }
 
 
-    DataElementcreateByHand( a, b, c, d, e, f ){//group, element, vr, vm, fieldBuffer, syntax
+    DataElementcreateByHand( group, element, vr, vm, valueField, syntax ){ //group, element, vr, vm, fieldBuffer, syntax
 
-        this.group = a;
-        this.element = b;
-        this.vr = c;
-        this.vm = d;
+        this.isDataElement = false;
 
-        this.field = new Buffer( e );
-        this.length = field.lengthBytes;
+        this.group   = group;
+        this.element = element;
+        this.vr      = vr;
+        this.vm      = vm;
 
-        this.syntax = f;
-        this.implicit = this.syntax == C.IMPLICIT_LITTLE_ENDIAN ? true : false;
-        this.endian = (this.syntax == C.IMPLICIT_LITTLE_ENDIAN || this.syntax == C.EXPLICIT_LITTLE_ENDIAN) ? C.LITTLE_ENDIAN : C.BIG_ENDIAN;
+        this.field   = new Buffer( valueField );
+        this.length  = field.lengthBytes;
+
+        //если syntax не задан, по умолчанию - DICOM Implicit VR Little Endian Transfer Syntax!!!
+          /*                                   */    this.syntax = C.IMPLICIT_LITTLE_ENDIAN;  this.implicit = true;  this.endian = C.LITTLE_ENDIAN;
+        if( syntax === C.EXPLICIT_LITTLE_ENDIAN )  { this.syntax = C.EXPLICIT_LITTLE_ENDIAN;  this.implicit = false; this.endian = C.LITTLE_ENDIAN; }
+        if( syntax === C.EXPLICIT_BIG_ENDIAN    )  { this.syntax = C.EXPLICIT_BIG_ENDIAN;     this.implicit = false; this.endian = C.BIG_ENDIAN; }
 
 
-        console.log('hand' + a + b + c + d + e);
+        this.isDataElement = false;
+
+        //console.log('hand' + a + b + c + d + e);
     }
 
     exit(){
 
+        this.isDataElement = false;
         console.log('error variable metod');
     }
 

4dd723ad709e05c1fa1b7b2e419f0cd6287d7089
mikivan1980 <mikivan1980@mail.ru>
2017-08-29 15:42:01 +0300

    добавлено поле implicit в класс родитель

--- _testing/DataSet.js
+++ _testing/DataSet.js
@@ -1,6 +1,33 @@
 /*
 * Итак, что мы делаем.
 *
+*
+* Существуют источники данных: файловая система, network.
+* Источник поставляет/принимает информацию (DICOM объекты) в виде потока байт.
+* Выделяют API для представления источника в Buffer.
+* let sourceBuffer = new Buffer( source ); - источник в виде буфера - потока байт
+*
+*
+* sourceBuffer либо оборачивается в поток чтения, либо подготавливается для дальнейшей передачи в источник.
+*
+* из потока чтения
+*
+*
+* let elem = new DataElement( DataSet );
+*
+* elem.readFrom(DataSet);
+*
+* elem.group === '0010'
+*
+* elem.writeTo( DataSet );
+*
+*
+*
+*
+*
+* ReadStream оборачивает любой вход в Buffer, все что съедобно для конструктора new Buffer( source )
+* можно подавать через source.
+*
 * isDataSet получает на вход stream, считается, что stream - буфер (Buffer) возвращенный например при чтении файлов DICOM,
 * функция устроена так, что на вход может быть подано все что угодно, содержание stream в теле функции помещается в буфер
 * т.е. вызывается конструктор new Buffer(stream).
@@ -9,16 +36,186 @@
 * Аварию вызовет то, что не сможет обернуть конструктор Buffer-а.
 ** А что об этом скажет документация?
 *
+* Так, время определиться с дизайном классов.
+*
 *
 * */
 
 
-const C = require('../abbot1Constants');
-const L = require('../abbot2Dictionary');
+const C   = require('../abbot1Constants');
+const L   = require('../abbot2Dictionary');
 const RW  = require('../abbot3Stream');
 
 
+//======================================================================================================================
+/* Тег элемента данных состоит из идентификатора группы и идентификатора элемента,
+* каждый тег идентифицирует единственую запись словаря данных.
+* чтобы ни было поддано на вход, значение true будет только для правильно заданных group, element
+* и при наличии этих значений в словаре данных.
+* Функции для применения в модуле, вне модуля не видна.
+* */
+function checkTag( group, element ){
+    return ( group in L.dicomDictionary ) ? ( element in L.dicomDictionary[group] ) :false;
+}
+
+
+//======================================================================================================================
+/* функция возвращает запись словаря сответствующую правильно заданным компонентам тега, иначе {}
+* Функции для применения в модуле, вне модуля не видна.
+* */
+function printTag( group, element ){
+    if( checkTag( group, element ) ) {
+        return L.dicomDictionary[group][element];}
+    else { return {}; }
+}
+
+
+//======================================================================================================================
+/* Функция  */
+function hypothesisExistenceDataElementOn( stream ){
+
+
+
+
+}
+
+
+
+//======================================================================================================================
+/*
+* Функции для применения в модуле, вне модуля не видна.
+* */
+function checkDataSet( stream ){
+    output = {
+        isDataSet    : false,
+        beginDataSet : null,
+        endDataSet   : null,
+        countLevel_0 : 0,
+        syntax       : null
+    };
+
+    //проверяем, что stream поток чтения, а не что-то другое.
+    let checkStream = ('name' in stream) ? ( stream.name === 'ReadStream' ) : false;
+
+    if( checkStream ) {
+
+        let offsetStream = stream.offset;
+        let endianStream = stream.endian;
+
+
+//входим в цикл while не достигнем конца stream
+
+        stream.setEndian(C.BIG_ENDIAN);
+
+        let groupTest   = paddingLeft("0000", stream.read(C.TYPE_UINT16).toString(16)).toUpperCase();
+        let elementTest = paddingLeft("0000", stream.read(C.TYPE_UINT16).toString(16)).toUpperCase();
+
+        let checkBigEndian = checkTag( groupTest, elementTest );
+
+        stream.increment(-4);
+
+        stream.setEndian(C.LITTLE_ENDIAN);
+
+        groupTest   = paddingLeft("0000", stream.read(C.TYPE_UINT16).toString(16)).toUpperCase();
+        elementTest = paddingLeft("0000", stream.read(C.TYPE_UINT16).toString(16)).toUpperCase();
+
+        let checkLittleEndian = checkTag( groupTest, elementTest );
+
+//получаем четыре возможных состояния
+// (checkBigEndian , checkLittleEndian) - (false,false) - следующий байт тоже и (true, true)?, хотя не столь очевидно что зднмь нет Data Element
+//(true,false)  or (false, true)
+
+
+
+
+
+
+        //stream.setEndian(this.endian);
+
+        srteam.offset = offsetStream;
+        stream.setEndian(endianStream);
+
+        return output;
+    }
+    else return output;
+}
+
+
+
+
+
+//??????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+console.log( checkTag('0008', '0006' ) );
+console.log( printTag('0008', '0006' ) );
+
+
 console.log('[DataSet]');
+
+
+
+class DataSet{
+    /* Экземпляр DataSet может быть получен из потока чтения или содержать хотябы один Data Element,
+    * поэтому конструктор принимает 'ReadSrteam' или 'DataElement' и ничего другого.
+    * */
+    constructor( stream, syntax ){
+
+        this.name = 'DataSet';
+
+        this.isDataSet1 = false;
+
+        this.syntax   = null;
+        this.endian   = null;
+        this.implicit = null;
+
+        this.bufferDataSet = null;
+
+        //проверяем, что первый аргумент поток чтения.
+        let checkStream = ('name' in stream) ? ( stream.name === 'ReadStream' ) : false;
+
+        if( checkStream ) {
+
+
+        }
+
+    }
+
+
+    selectDataElement( group, element ){
+        /*по заданным group, element ищем в this.bufferDataSet  элемент данны и возвращает его*/
+
+    }
+
+    updateDataElement(DataElement){
+        /*bmbm*/
+
+
+
+    }
+
+    insertDataElement(DataElement){
+
+
+
+    }
+
+    deleteDataElement( group, element ){
+
+
+
+    }
+
+    next(){
+
+
+    }
+
+
+}
+module.exports.DataSet = DataSet;
+
+
+
+
 
 //isDataSet(fs, 'File');
 //isDataSet(ns, 'Net Stream');
@@ -43,7 +240,10 @@ function isDataSet( stream, typeSource ){
         let checkSign = (readStream.readString(4,C.TYPE_ASCII) === 'DICM');
 
 
-        console.log( checkPrefix   + '  ;  ' + checkSign );
+        checkDataElement = checkPrefix&&checkSign;
+
+
+        console.log( checkPrefix + '  ;  ' + checkSign );
 
 
     }
@@ -68,10 +268,10 @@ function isDataSet( stream, typeSource ){
 
 const fs = require('fs');
 
-let DataSet = new Buffer( fs.readFileSync('../_testing/dcm/_test_mr.dcm') );
+let DataSet1 = new Buffer( fs.readFileSync('D://dop/node.js/abbot/_testing/dcm/_test_mr.dcm') );
 
 
-//isDataSet(DataSet,'File');
+isDataSet(DataSet1,'File');
 
-isDataSet('tyritvurhtgjkrn','File');
+//isDataSet('tyritvurhtgjkrn','File');
 
